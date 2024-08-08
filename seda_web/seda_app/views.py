8882d1e6546c85c304747dbd0998a8ec67d9fc45
fbucko <filipbucko369@gmail.com>
2024-08-08 03:00:03 +0200

    ADD: Small fixes

--- seda_web/seda_app/views.py
+++ seda_web/seda_app/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.utils.text import slugify
 
 # Create your views here.
 def main_page(request):
-    return render(request, 'base.html')
+    brands_categories = [
+        "Drinking water",
+        "Water Management",
+        "Feeding & Oil",
+        "Distilation, Brewing & Juice",
+        "Solar",        
+    ]
+    # Categories for div classes
+    slugified_categories = [slugify(category) for category in brands_categories]
+    brand_categories = zip(brands_categories, slugified_categories)
+    context = {
+        "brands_categories" : brands_categories,
+        "slugified_categories" : slugified_categories,
+        "brand_categories" : brand_categories
+    }
+    return render(request, 'base.html', context)
